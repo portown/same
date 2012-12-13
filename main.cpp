75d173890e0ca5e11f08224287c16559ffa05f27
YASUDA Jiro <portown09@gmail.com>
2012-12-13 23:12:59 +0900

    The first commit.

---
+++ main.cpp
@@ -0,0 +1,241 @@
+// main.cpp
+
+#include "common.h"
+
+// 定数の定義
+#define CLSNAME ( LPCTSTR )"SAMEGAME"
+#define WNDNAME ( LPCTSTR )"さめがめー"
+#define WINX 640
+#define WINY 480
+#define GAMEX 320
+#define GAMEY 480
+
+// 関数のプロトタイプ宣言
+LRESULT CALLBACK WndProc( HWND, UINT, WPARAM, LPARAM );
+BOOL InitApp( HINSTANCE );
+BOOL InitInstance( HINSTANCE, int );
+int Run( void );
+
+
+// ==============================================
+// 実装
+// ==============================================
+
+// エントリポイント
+int WINAPI WinMain( HINSTANCE hCurInst, HINSTANCE hPrev, LPSTR lpCmd, int nCmd )
+{
+	int iRet;
+	
+	CreateMutex( NULL, FALSE, CLSNAME );
+	if ( GetLastError() == ERROR_ALREADY_EXISTS ) return 0;
+
+	if ( !SetCurDir() ) return 0;
+
+	if ( !InitApp( hCurInst ) ) return 0;
+	if ( !InitInstance( hCurInst, nCmd ) ) return 0;
+
+	iRet = Run();
+	
+	return iRet;
+}
+
+// ウィンドウクラスの登録
+BOOL InitApp( HINSTANCE hInst )
+{
+	WNDCLASSEX wc;
+
+	wc.cbSize = sizeof( WNDCLASSEX );
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = ( HBRUSH )GetStockObject( WHITE_BRUSH );
+	wc.hCursor = ( HCURSOR )LoadImage( hInst, MAKEINTRESOURCE( IDC_SAME ),
+		IMAGE_CURSOR, 0, 0, LR_DEFAULTSIZE | LR_SHARED );
+	wc.hIcon = ( HICON )LoadImage( hInst, MAKEINTRESOURCE( IDI_SAME ),
+		IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_SHARED );
+	wc.hIconSm = ( HICON )LoadImage( hInst, MAKEINTRESOURCE( IDI_SAME ),
+		IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_SHARED );
+	wc.hInstance = hInst;
+	wc.lpfnWndProc = ( WNDPROC )WndProc;
+	wc.lpszClassName = CLSNAME;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	if ( !RegisterClassEx( &wc ) )
+		return FALSE;
+
+	return TRUE;
+}
+
+// ウィンドウの作成
+BOOL InitInstance( HINSTANCE hInst, int nCmd )
+{
+	HWND hWnd;
+	SIZE szWnd;
+
+	szWnd.cx = WINX + GetSystemMetrics( SM_CXFIXEDFRAME ) * 2;
+	szWnd.cy = WINY + GetSystemMetrics( SM_CYFIXEDFRAME ) * 2 + GetSystemMetrics( SM_CYCAPTION );
+
+	hWnd = CreateWindowEx( 0,
+		CLSNAME,
+		WNDNAME,
+		WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX,
+		( GetSystemMetrics( SM_CXSCREEN ) - szWnd.cx ) / 2,
+		( GetSystemMetrics( SM_CYSCREEN ) - szWnd.cy ) / 2,
+		szWnd.cx,
+		szWnd.cy,
+		NULL,
+		NULL,
+		hInst,
+		NULL );
+
+	if ( !hWnd ) return FALSE;
+
+	ShowWindow( hWnd, nCmd );
+	UpdateWindow( hWnd );
+
+	return TRUE;
+}
+
+// メッセージ・ループ
+int Run( void )
+{
+	MSG msg;
+
+	while ( GetMessage( &msg, NULL, 0, 0L ) > 0 )
+	{
+		TranslateMessage( &msg );
+		DispatchMessage( &msg );
+	}
+
+	return ( int )msg.wParam;
+}
+
+// ウィンドウプロシージャ
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wp, LPARAM lp )
+{
+	static HBITMAP s_hBackBm;
+	static CGAME *s_pcGame = NULL;
+	static HDC s_hBackDC;
+	PAINTSTRUCT ps;
+	POINT pt;
+	HDC hDC;
+	unsigned char ucRet;
+
+	switch ( msg )
+	{
+		case WM_CREATE:
+			InitSurface( s_hBackDC, s_hBackBm, WINX, WINY );
+			s_pcGame = new CMENU( WINX, WINY );
+			break;
+			
+		case WM_PAINT:
+			hDC = BeginPaint( hWnd, &ps );
+			PatBlt( s_hBackDC, 0, 0, WINX, WINY, BLACKNESS );
+			s_pcGame->Draw( s_hBackDC );
+			BitBlt( hDC, 0, 0, WINX, WINY, s_hBackDC, 0, 0, SRCCOPY );
+			EndPaint( hWnd, &ps );
+			break;
+
+		case WM_MOUSEMOVE:
+			pt.x = LOWORD( lp );
+			pt.y = HIWORD( lp );
+			s_pcGame->Select( pt );
+			InvalidateRect( hWnd, NULL, FALSE );
+			break;
+
+		case WM_LBUTTONUP:
+			ucRet = s_pcGame->Click();
+			switch ( ucRet )
+			{
+				case CR_ENDGAME:
+					DestroyWindow( hWnd );
+					break;
+
+				case CR_TITLEMENU:
+					_DELETE( s_pcGame );
+					s_pcGame = new CMENU( WINX, WINY );
+					break;
+
+				case CR_BEGINNORMAL:
+				case CR_BEGINMASK1:
+				case CR_BEGINMASK2:
+				case CR_BEGINMASK3:
+				case CR_BEGINMASK4:
+					_DELETE( s_pcGame );
+					s_pcGame = new CSAME( GAMEX, GAMEY, ucRet - CR_BEGINNORMAL );
+					break;
+
+				case CR_REPLAY:
+				case CR_REPLAY0:
+				case CR_REPLAY1:
+				case CR_REPLAY2:
+				case CR_REPLAY3:
+				case CR_REPLAY4:
+				case CR_REPLAY5:
+				case CR_REPLAY6:
+				case CR_REPLAY7:
+				case CR_REPLAY8:
+				case CR_REPLAY9:
+					_DELETE( s_pcGame );
+					s_pcGame = new CREPLAY( hWnd, GAMEX, GAMEY, ucRet - CR_REPLAY0 );
+					break;
+
+				case CR_CRITICALERROR:
+					MessageBox( hWnd, "致命的なエラーが発生しました",
+						"強制終了", MB_OK | MB_ICONEXCLAMATION );
+					DestroyWindow( hWnd );
+					break;
+			}
+			InvalidateRect( hWnd, NULL, FALSE );
+			break;
+
+		case WM_KEYDOWN:
+			ucRet = s_pcGame->KeyDown( wp );
+			switch ( ucRet )
+			{
+				case CR_TITLEMENU:
+					_DELETE( s_pcGame );
+					s_pcGame = new CMENU( WINX, WINY );
+					break;
+
+				case CR_BEGINNORMAL:
+				case CR_BEGINMASK1:
+				case CR_BEGINMASK2:
+				case CR_BEGINMASK3:
+				case CR_BEGINMASK4:
+					_DELETE( s_pcGame );
+					s_pcGame = new CSAME( GAMEX, GAMEY, ucRet - CR_BEGINNORMAL );
+					break;
+
+				case CR_ENDGAME:
+					DestroyWindow( hWnd );
+					break;
+
+				default:
+					return DefWindowProc( hWnd, msg, wp, lp );
+			}
+			InvalidateRect( hWnd, NULL, FALSE );
+			break;
+
+		case WM_TIMER:
+			if ( wp == MINE_TIMER )
+			{
+				( ( CREPLAY * )s_pcGame )->Replay();
+			}
+			break;
+
+		case WM_DESTROY:
+			_DELETE( s_pcGame );
+			RelsSurface( s_hBackDC, s_hBackBm );
+			PostQuitMessage( 0 );
+			break;
+
+		default:
+			return DefWindowProc( hWnd, msg, wp, lp );
+	}
+
+	return 0L;
+}
+
+
+// EOF
---
+++ replay.cpp
@@ -0,0 +1,530 @@
+// replay.cpp
+
+#include "common.h"
+
+
+// ==============================================
+// 実装
+// ==============================================
+
+// 初期化
+CREPLAY::CREPLAY( HWND hWnd, unsigned short wx, unsigned short wy, char cNum )
+{
+	unsigned short i, j;
+
+	m_Status = GS_PLAYING;
+
+	m_Width = wx / PIX;
+	m_Height = wy / PIY;
+	m_bx = m_Width;
+	m_by = m_Height;
+
+	SetRect( &m_rcArea, 0, 0, wx, wy );
+
+	m_Num = 0;
+	m_Tries = 0;
+	m_Pieces = m_Width * m_Height;
+	m_Groups = 0;
+	m_Score = 0;
+
+	m_Played.clear();
+	m_GameNum = 0;
+	if ( cNum == -1 ) cNum = '\0';
+	else cNum += '0';
+	if ( !LoadReplay( cNum ) )
+	{
+		m_Status = GS_NOREPLAY;
+		return;
+	}
+	init_genrand( m_GameNum );
+	m_Tries = 0;
+	m_bErase = false;
+	
+	m_Area = new unsigned char [m_Width * m_Height];
+	for ( i = 0; i < m_Height; ++i )
+	{
+		for ( j = 0; j < m_Width; ++j )
+		{
+			m_Area[i * m_Width + j] = ( unsigned char )( genrand_int32() % 5 + 1 );
+		}
+	}
+
+	CntGroups();
+
+	m_hDC = CreateCompatibleDC( NULL );
+	m_hBm = Load_Bmp( DATA( system.bmp ) );
+	SelectObject( m_hDC, m_hBm );
+
+	m_hCurDC = CreateCompatibleDC( m_hDC );
+	m_hCurBm = ( HBITMAP )LoadImage( ( HINSTANCE )GetWindowLongPtr( hWnd, GWLP_HINSTANCE ),
+		MAKEINTRESOURCE( IDB_SAMECUR ), IMAGE_BITMAP, 64, 32, LR_SHARED );
+	SelectObject( m_hCurDC, m_hCurBm );
+
+	m_hWnd = hWnd;
+	SetTimer( m_hWnd, MINE_TIMER, 1000, NULL );
+}
+
+// マス目の描画
+void CREPLAY::Draw( HDC hDC )
+{
+	unsigned short i, j;
+	unsigned char tmp;
+	char strTmp[0x100];
+
+	if ( m_Status == GS_NOREPLAY )
+	{
+		PutText( hDC, 320 - 13 * 20, 220, 40, RGB( 255, 255, 255 ), "リプレイデータがありません", 26 );
+		PutText( hDC, 320, 272, 20, RGB( 255, 255, 255 ), "クリックでタイトルに戻る", 24 );
+		return;
+	}
+
+	// ゲーム盤の描画
+	for ( i = 0; i < m_Height; ++i )
+	{
+		for ( j = 0; j < m_Width; ++j )
+		{
+			tmp = m_Area[i * m_Width + j];
+			
+			if ( tmp != 0 )
+			{
+				--tmp;
+				
+				if ( tmp & ( unsigned char )0x80 )
+				{
+					tmp = tmp ^ ( unsigned char )0x80;
+					
+					BitBlt( hDC, j * PIX, i * PIY, PIX, PIY, m_hDC, tmp * PIX, PIY * 2, SRCAND );
+					BitBlt( hDC, j * PIX, i * PIY, PIX, PIY, m_hDC, tmp * PIX, PIY, SRCPAINT );
+				}
+				else
+				{
+					BitBlt( hDC, j * PIX, i * PIY, PIX, PIY, m_hDC, tmp * 32, PIY * 2, SRCAND );
+					BitBlt( hDC, j * PIX, i * PIY, PIX, PIY, m_hDC, tmp * 32, 0, SRCPAINT );
+				}
+			}
+		}
+	}
+
+	// カーソルの描画
+	if ( m_bx < m_Width && m_by < m_Height )
+	{
+		BitBlt( hDC, m_bx * 32 + PIX / 2, m_by * 32 + PIY / 2, 32, 32, m_hCurDC, 32, 0, SRCAND );
+		BitBlt( hDC, m_bx * 32 + PIX / 2, m_by * 32 + PIY / 2, 32, 32, m_hCurDC, 0, 0, SRCPAINT );
+	}
+
+	// その他の描画
+	wsprintf( strTmp, "　　スコア：%lu", m_Score );
+	PutText( hDC, m_rcArea.right, 0, 20, RGB( 255, 255, 255 ), strTmp );
+	PutText( hDC, m_rcArea.right + 80, 24, 24, RGB( 255, 255, 255 ), "- リプレイ -" );
+
+	wsprintf( strTmp, "選択エリア：%u", m_Num );
+	PutText( hDC, m_rcArea.right, 60, 20, RGB( 255, 255, 255 ), strTmp );
+	wsprintf( strTmp, "　　　手数：%u", m_Tries );
+	PutText( hDC, m_rcArea.right, 80, 20, RGB( 255, 255, 255 ), strTmp );
+	wsprintf( strTmp, "　残り個数：%u", m_Pieces );
+	PutText( hDC, m_rcArea.right, 100, 20, RGB( 255, 255, 255 ), strTmp );
+	wsprintf( strTmp, "　残り塊数：%u", m_Groups );
+	PutText( hDC, m_rcArea.right, 120, 20, RGB( 255, 255, 255 ), strTmp );
+
+	wsprintf( strTmp, "　　 X座標：%u", ( m_bx < m_Width ? m_bx : 0 ) );
+	PutText( hDC, m_rcArea.right + 152, 60, 20, RGB( 255, 255, 255 ), strTmp );
+	wsprintf( strTmp, "　　 Y座標：%u", ( m_by < m_Height ? m_by : 0 ) );
+	PutText( hDC, m_rcArea.right + 152, 80, 20, RGB( 255, 255, 255 ), strTmp );
+
+	wsprintf( strTmp, "Game Number:%lu", m_GameNum );
+	PutText( hDC, m_rcArea.right + 160, m_rcArea.bottom - 12, 12, RGB( 255, 255, 255 ), strTmp );
+
+	// クリア後の描画
+	if ( m_Status == GS_CLEAR || m_Status == GS_ALLCLEAR )
+	{
+		if ( m_Status == GS_CLEAR )
+			PutText( hDC, m_rcArea.right + 100, 180, 40, RGB( 255, 255, 255 ), "終了！" );
+		else
+			PutText( hDC, m_rcArea.right + 80, 180, 40, RGB( 255, 255, 255 ), "全消し！" );
+
+		PutText( hDC, m_rcArea.right, 280, 20, RGB( 255, 255, 255 ), "F12  :メニューに戻る" );
+		PutText( hDC, m_rcArea.right, 300, 20, RGB( 255, 255, 255 ), "Esc  :終了" );
+
+		PutText( hDC, m_rcArea.right, 340, 20, RGB( 255, 255, 255 ), "キーボードの0～9を押すと" );
+		PutText( hDC, m_rcArea.right + 40, 360, 20, RGB( 255, 255, 255 ), "リプレイを保存します" );
+	}
+}
+
+// マウス移動
+void CREPLAY::Select( POINT pt )
+{
+}
+
+// マス目チェック
+void CREPLAY::Onselect( unsigned short pos )
+{
+	static unsigned short s_sBef = 0;
+	unsigned short x, y;
+
+	if ( m_Status != GS_PLAYING ) return;
+
+	Inexplore( m_by * m_Width + m_bx );
+	m_Num = 0;
+	m_bx = 0;
+	m_by = 0;
+
+	x = pos % m_Width;
+	y = pos / m_Width;
+
+	if ( pos < 0 ) return;
+	if ( pos >= m_Width * m_Height ) return;
+	if ( m_Area[pos] == 0 ) return;
+	Explore( pos, m_Area[pos] );
+
+	m_bx = x;
+	m_by = y;
+
+	if ( m_Num == 1 ) Unselect();
+
+	if ( m_Area[pos] & 0x80 )
+	{
+		if ( !( m_Area[s_sBef] & 0x80 ) )
+		{
+			PlaySound( SELWAV, NULL, SND_FILENAME | SND_ASYNC );
+		}
+	}
+	s_sBef = pos;
+}
+
+// マス目チェック・再帰
+void CREPLAY::Explore( unsigned short pos, unsigned char piece )
+{
+	if ( pos >= m_Width * m_Height ) return;
+	if ( piece == 0 ) return;
+	if ( m_Area[pos] & ( unsigned char )0x80 ) return;
+
+	if ( m_Area[pos] == piece )
+	{
+		m_Area[pos] |= ( unsigned char )0x80;
+		++m_Num;
+
+		if ( pos >= m_Width ) Explore( pos - m_Width, piece );
+		if ( ( pos % m_Width ) != 0 ) Explore( pos - 1, piece );
+		if ( ( pos % m_Width ) < m_Width - 1 ) Explore( pos + 1, piece );
+		if ( ( pos / m_Width ) < m_Height - 1 ) Explore( pos + m_Width, piece );
+	}
+}
+
+// マス目選択消去
+void CREPLAY::Unselect( void )
+{
+	Inexplore( m_by * m_Width + m_bx );
+}
+
+// マス目チェック・反転再帰
+void CREPLAY::Inexplore( unsigned short pos )
+{
+	if ( pos >= m_Width * m_Height ) return;
+	if ( m_Area[pos] & ( unsigned char )0x80 )
+	{
+		m_Area[pos] ^= ( unsigned char )0x80;
+
+		if ( pos >= m_Width ) Inexplore( pos - m_Width );
+		if ( ( pos % m_Width ) != 0 ) Inexplore( pos - 1 );
+		if ( ( pos % m_Width ) < m_Width - 1 ) Inexplore( pos + 1 );
+		if ( ( pos / m_Width ) < m_Height - 1 ) Inexplore( pos + m_Width );
+	}
+}
+
+// クリック
+unsigned char CREPLAY::Click( void )
+{
+	if ( m_Status == GS_NOREPLAY )
+		return CR_TITLEMENU;
+
+	return CR_NOSTATUS;
+}
+
+// 駒消去
+unsigned char CREPLAY::Onclick( void )
+{
+	unsigned char cRet;
+
+	if ( m_Status != GS_PLAYING ) return CR_NOSTATUS;
+
+	if ( m_Num < 2 ) return CR_NOSTATUS;
+	if ( m_bx >= m_Width || m_by >= m_Height ) return CR_NOSTATUS;
+	if ( m_Area[m_by * m_Width + m_bx] == 0 ) return CR_NOSTATUS;
+	
+	Exexplore( m_by * m_Width + m_bx );
+
+	++m_Tries;
+	m_Pieces -= m_Num;
+	AddScore( ADDSCORE( m_Num ) );
+
+	Check();
+
+	cRet = EndCheck();
+	switch ( cRet )
+	{
+		case CR_NOSTATUS:
+			PlaySound( ERSWAV, NULL, SND_FILENAME | SND_SYNC );
+			m_Num = 0;
+			Explore( m_by * m_Width + m_bx, m_Area[m_by * m_Width + m_bx] );
+			if ( m_Num == 1 ) Unselect();
+			break;
+
+		case CR_CLEAR:
+			PlaySound( CLRWAV, NULL, SND_FILENAME | SND_ASYNC );
+			m_Status = GS_CLEAR;
+			break;
+
+		case CR_ALLCLEAR:
+			AddScore( 1000000 );
+			PlaySound( CLRWAV, NULL, SND_FILENAME | SND_ASYNC );
+			m_Status = GS_ALLCLEAR;
+			break;
+	}
+
+	return cRet;
+}
+
+// 駒消去・再帰
+void CREPLAY::Exexplore( unsigned short pos )
+{
+	if ( pos >= m_Width * m_Height ) return;
+	
+	if ( m_Area[pos] & ( unsigned char )0x80 )
+	{
+		m_Area[pos] = 0;
+		
+		if ( pos >= m_Width ) Exexplore( pos - m_Width );
+		if ( ( pos % m_Width ) != 0 ) Exexplore( pos - 1 );
+		if ( ( pos % m_Width ) < m_Width - 1 ) Exexplore( pos + 1 );
+		if ( ( pos / m_Width ) < m_Height - 1 ) Exexplore( pos + m_Width );
+	}
+}
+
+// 駒落ちチェック
+void CREPLAY::Check( void )
+{
+	unsigned short i, j, max;
+	
+	max = m_Width * m_Height;
+	
+	for ( i = 0; i < max; ++i )
+	{
+		if ( m_Area[i] == 0 ) VShift( i );
+	}
+	
+	for ( i = 0; i < m_Width - 1; ++i )
+	{
+		if ( m_Area[( m_Height - 1 ) * m_Width + i] == 0 )
+		{
+			for ( j = i; j < m_Width; ++j )
+			{
+				if ( m_Area[( m_Height - 1 ) * m_Width + j] != 0 )
+					break;
+			}
+
+			if ( j == m_Width ) break;;
+
+			for ( j = 0; j < m_Height; ++j )
+				HShift( j * m_Width + i );
+
+			--i;
+		}
+	}
+}
+
+// 縦方向シフト・再帰
+void CREPLAY::VShift( unsigned short pos )
+{
+	if ( pos >= m_Width * m_Height ) return;
+	
+	if ( pos >= m_Width )
+	{
+		m_Area[pos] = m_Area[pos - m_Width];
+		VShift( pos - m_Width );
+	}
+	else
+		m_Area[pos] = 0;
+}
+
+// 横方向シフト・再帰
+void CREPLAY::HShift( unsigned short pos )
+{
+	if ( pos >= m_Width * m_Height ) return;
+	
+	if ( pos % m_Width < m_Width - 1 )
+	{
+		m_Area[pos] = m_Area[pos + 1];
+		HShift( pos + 1 );
+	}
+	else
+		m_Area[pos] = 0;
+}
+
+// 終了チェック
+unsigned char CREPLAY::EndCheck( void )
+{
+	bool bAll;
+
+	bAll = CntGroups();
+
+	if ( bAll ) return CR_ALLCLEAR;
+	else if ( m_Groups == 0 ) return CR_CLEAR;
+
+	return CR_NOSTATUS;
+}
+
+// 塊の個数計算
+bool CREPLAY::CntGroups( void )
+{
+	unsigned short max, i;
+	unsigned char cPiece = 0;
+
+	max = m_Width * m_Height;
+	m_Groups = 0;
+
+	for ( i = 0; i < max; ++i )
+	{
+		cPiece |= ( m_Area[i] != 0 );
+		if ( m_Area[i] == 0 ) continue;
+		if ( m_Area[i] & ( unsigned char )0x80 ) continue;
+
+		m_Num = 0;
+		Explore( i, m_Area[i] );
+		if ( m_Num > 1 )
+		{
+			++m_Groups;
+		}
+	}
+
+	// 後始末
+	for ( i = 0; i < max; ++i )
+	{
+		if ( m_Area[i] == 0 ) continue;
+		if ( !( m_Area[i] & ( unsigned char )0x80 ) ) continue;
+
+		Inexplore( i );
+	}
+
+	return cPiece == 0;
+}
+
+// スコア加算
+void CREPLAY::AddScore( unsigned long add )
+{
+	m_Score += add;
+}
+
+// キーダウン
+unsigned char CREPLAY::KeyDown( WPARAM key )
+{
+	switch ( key )
+	{
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			SaveReplay( ( char )key );
+			break;
+
+		case VK_F12:
+			return CR_TITLEMENU;
+
+		case VK_ESCAPE:
+			return CR_ENDGAME;
+	}
+
+	return CR_NOSTATUS;
+}
+
+// リプレイ再生中
+void CREPLAY::Replay( void )
+{
+	unsigned short i = ( unsigned short )m_Played.size();
+	if ( m_Tries > i ) return;
+
+	if ( m_bErase )
+	{
+		Onclick();
+		m_bErase = false;
+	}
+	else
+	{
+		Onselect( m_Played[m_Tries] );
+		m_bErase = true;
+	}
+
+	InvalidateRect( m_hWnd, NULL, FALSE );
+}
+
+// デストラクタ
+CREPLAY::~CREPLAY( void )
+{
+	KillTimer( m_hWnd, MINE_TIMER );
+
+	RelsSurface( m_hCurDC, m_hCurBm );
+	RelsSurface( m_hDC, m_hBm );
+	delete [] m_Area;
+}
+
+// リプレイデータ読み込み
+bool CREPLAY::LoadReplay( char cNum )
+{
+	HANDLE hFile;
+	DWORD dwRead;
+	unsigned short i;
+	unsigned char ucDat;
+	char strFName[0x100];
+
+	lstrcpy( strFName, REPFILE );
+	strFName[lstrlen( strFName ) + 1] = '\0';
+	strFName[lstrlen( strFName )] = cNum;
+
+	hFile = CreateFile( strFName, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL );
+	if ( hFile == INVALID_HANDLE_VALUE ) return false;
+
+	SetFilePointer( hFile, 0, NULL, FILE_BEGIN );
+	ReadFile( hFile, ( LPVOID )&m_GameNum, sizeof( unsigned long ), &dwRead, NULL );
+	if ( dwRead != sizeof( unsigned long ) ) return false;
+
+	ReadFile( hFile, ( LPVOID )&m_Tries, sizeof( unsigned short ), &dwRead, NULL );
+	if ( dwRead != sizeof( unsigned short ) ) return false;
+
+	for ( i = 0; i < m_Tries; ++i )
+	{
+		ReadFile( hFile, ( LPVOID )&ucDat, 1, &dwRead, NULL );
+		if ( dwRead != 1 ) return false;
+		m_Played.push_back( ucDat );
+	}
+
+	CloseHandle( hFile );
+
+	m_cRepNum = cNum;
+
+	return true;
+}
+
+// リプレイデータ書き込み
+void CREPLAY::SaveReplay( char cNum )
+{
+	char strSrcName[0x100], strDstName[0x100];
+
+	lstrcpy( strSrcName, REPFILE );
+	strSrcName[lstrlen( strSrcName ) + 1] = '\0';
+	strSrcName[lstrlen( strSrcName )] = m_cRepNum;
+
+	lstrcpy( strDstName, REPFILE );
+	strDstName[lstrlen( strDstName ) + 1] = '\0';
+	strDstName[lstrlen( strDstName )] = cNum;
+
+	CopyFile( strSrcName, strDstName, TRUE );
+
+}
+
+
+// EOF
