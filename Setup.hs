3f126404197670b516e748c1ededadb07d29656c
YASUDA Jiro <portown09@gmail.com>
2016-11-04 15:26:33 +0900

    Link res file

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,16 @@
+import Distribution.PackageDescription (PackageDescription(extraSrcFiles))
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo)
+import Distribution.Simple.Setup (BuildFlags(..), fromFlagOrDefault)
+import Distribution.Simple.Utils (createDirectoryIfMissingVerbose)
+import qualified Distribution.Verbosity as V
+import System.Process (system)
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks {buildHook = resourceHook}
+
+resourceHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
+resourceHook packageDesc localBuildInfo userHooks buildFlags = do
+        createDirectoryIfMissingVerbose (fromFlagOrDefault V.silent $ buildVerbosity buildFlags) False "dist"
+        system "windres src-c/same.rc dist/resource.o"
+        (buildHook simpleUserHooks) packageDesc localBuildInfo userHooks buildFlags
